b99a7a55c933fbd73f72311ba2d534850603b5c3
Nadwey <81181783+Nadwey@users.noreply.github.com>
2022-09-13 21:44:49 +0200

    Add basic character support
    - Add basic character support
    - Add jesse pinkman (supposed to be sleeping)
    - Add 2 more backgrounds

---
+++ Source/WlodzimierzBialy/Private/Character2D.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character2D.h"
+#include "Kismet/KismetMathLibrary.h"
+
+// Sets default values
+ACharacter2D::ACharacter2D()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACharacter2D::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACharacter2D::Tick(float DeltaTime)
+{
+	FVector playerLocation = GetWorld()->GetFirstPlayerController()->GetPawn()->GetActorLocation();
+	float newYawRotation = UKismetMathLibrary::FindLookAtRotation(this->GetActorLocation(), playerLocation).Yaw;
+	FRotator newRotation = this->GetActorRotation();
+	newRotation.Yaw = newYawRotation - 90;
+
+	this->SetActorRotation(newRotation);
+
+	Super::Tick(DeltaTime);
+}
